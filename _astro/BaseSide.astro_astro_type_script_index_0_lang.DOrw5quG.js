bea7738e74903da9fbf30db7c8048cf7dca18952
gkzhb <gkzhb@users.noreply.github.com>
2025-02-13 05:12:06 +0000

    fix: gh action pnpm version gkzhb/astro-blog@e73c0afe2dfca816e4943f83dc50ba4c046d2801

---
+++ _astro/BaseSide.astro_astro_type_script_index_0_lang.DOrw5quG.js
@@ -0,0 +1,1 @@
+import{h as t}from"./handle-toggle-click.7U0XlR-S.js";const e=()=>{document.getElementById("theme-toggle")?.addEventListener("click",t)};e();document.addEventListener("astro:after-swap",e);
